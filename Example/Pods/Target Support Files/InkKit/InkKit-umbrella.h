31560ccebc9a6cc6cfd62af9613813cc7c10cffd
Shaps <shapsuk@me.com>
2016-04-09 14:42:15 +0100

    Removed old project files

--- Example/Pods/Target Support Files/InkKit/InkKit-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double InkKitVersionNumber;
-FOUNDATION_EXPORT const unsigned char InkKitVersionString[];
-
--- Example/Pods/Target Support Files/Pods-InkExampleiOS/Pods-InkExampleiOS-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_InkExampleiOSVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_InkExampleiOSVersionString[];
-

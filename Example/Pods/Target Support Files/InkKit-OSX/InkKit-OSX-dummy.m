27bf8bbe7a3165b7eab5e07679e56b62a11d65a5
Shaps Benkau <shapsuk@me.com>
2016-09-30 14:34:40 +0100

    Added missing OSX files to repo

---
+++ Example/Pods/Target Support Files/InkKit-OSX/InkKit-OSX-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_InkKit_OSX : NSObject
+@end
+@implementation PodsDummy_InkKit_OSX
+@end
---
+++ Example/Pods/Target Support Files/InkKit-iOS/InkKit-iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_InkKit_iOS : NSObject
+@end
+@implementation PodsDummy_InkKit_iOS
+@end
